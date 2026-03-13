38cf80db159cedc58424d5f0e2bd63b2215d7d68
EVOSGPT. AI <176422595+CalamitousLightning@users.noreply.github.com>
2026-03-13 12:29:27 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ from .routes import provider_routes
 from .limiter import limiter
 
 from slowapi.errors import RateLimitExceeded
-from slowapi.middleware import SlowAPMiddleware
+from slowapi.middleware import SlowAPIMiddleware
 
 
 app = FastAPI(
